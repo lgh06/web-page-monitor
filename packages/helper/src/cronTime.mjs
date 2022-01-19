8e50023112684a9b17d7201cc49f6199b1a399d6
Daniel Liu <hnnk@qq.com>
2022-01-19 13:44:57 +0800

    move jsdoc to d.ts

--- packages/helper/src/cronTime.mjs
+++ packages/helper/src/cronTime.mjs
@@ -1,10 +1,5 @@
 import { CronTime as CT } from "cron"
-/**
- * get next 3 timestamps (13 digit, miliseconds) from cron syntax
- * UTC timestamp
- * @param {string} cron cron syntax,see https://github.com/kelektiv/node-cron/blob/5626867f67d80cce411d2b0f14f3a64063df99c6/lib/time.js#L148
- * @returns {Array} 3 timestamps (13 digit, miliseconds)
- */
+
  function getNextTimes(cron){
   let ctArr = new CT(cron).sendAt(3);
   // UTC timestamp in miliseconds / English time
@@ -17,14 +12,7 @@ import { CronTime as CT } from "cron"
   return convertedArr
 }
 
-  // 
-  /**
-   * Calculate a ISO string in local time, minute, without end 'Z'
-   * 
-   * @param {Date} oneDate 
-   * @param {number} plusMinutes 
-   * @returns a string, in your timezone 2022-01-19T13:14
-   */
+
   function toLocalISOString(oneDate, plusMinutes = 0){
     let offset = oneDate.getTimezoneOffset();
     return new Date(oneDate.setMinutes(oneDate.getMinutes()-offset + plusMinutes)).toISOString().substring(0, 16)
--- packages/helper/src/helper.d.ts
+++ packages/helper/src/helper.d.ts
@@ -1,6 +1,19 @@
 // https://www.typescriptlang.org/docs/handbook/declaration-files/templates/module-d-ts.html#types-in-modules
 export namespace  CronTime {
+  /**
+   * get next 3 timestamps (13 digit, miliseconds) from cron syntax
+   * UTC timestamp
+   * @param {string} cron cron syntax,see https://github.com/kelektiv/node-cron/blob/5626867f67d80cce411d2b0f14f3a64063df99c6/lib/time.js#L148
+   * @returns {Array} 3 timestamps (13 digit, miliseconds)
+   */
   function getNextTimes (cron: string): Array<number>;
+  /**
+   * Calculate a ISO string in local time, minute, without end 'Z'
+   * 
+   * @param {Date} oneDate 
+   * @param {number} plusMinutes 
+   * @returns a string, in your timezone 2022-01-19T13:14
+   */
   function toLocalISOString(oneDate: Date, plusMinutes = 0):string;
 } 
 
