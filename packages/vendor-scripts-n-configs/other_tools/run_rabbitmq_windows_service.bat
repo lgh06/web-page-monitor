b445b288b12255f03000e5e514f1c0ad92efe46d
Daniel Liu <hnnk@qq.com>
2022-01-22 12:01:09 +0800

    remove no use chinese comments in scripts

--- packages/vendor-scripts-n-configs/other_tools/run_rabbitmq_windows_service.bat
+++ packages/vendor-scripts-n-configs/other_tools/run_rabbitmq_windows_service.bat
@@ -1,6 +1,4 @@
 rem Open this file in ANSI / GBK / GB2312 encoding.
-@rem Mongodb官网上的windows版本安装包，默认为网络服务
-@rem 若没有运行，执行此脚本可以运行mongdodb的网络服务
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 cd /d "C:\Program Files\RabbitMQ Server\rabbitmq_server-3.9.13\sbin"
 "./rabbitmq-service.bat" start
--- packages/vendor-scripts-n-configs/other_tools/stop_mongodb_windows_net_service.bat
+++ packages/vendor-scripts-n-configs/other_tools/stop_mongodb_windows_net_service.bat
@@ -1,5 +1,3 @@
 rem Open this file in ANSI / GBK / GB2312 encoding.
-@rem Mongodb官网上的windows版本安装包，默认为网络服务
-@rem 若没有运行，执行此脚本可以停止mongdodb的网络服务
 %1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("cmd.exe","/c %~s0 ::","","runas",1)(window.close)&&exit
 net stop mongodb
