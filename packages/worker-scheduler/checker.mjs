e961fd93cb8de404680d311807111f473c8917d2
Daniel Liu <hnnk@qq.com>
2022-01-24 07:37:58 +0800

    calculate worked

--- packages/worker-scheduler/checker.mjs
+++ packages/worker-scheduler/checker.mjs
@@ -1,10 +1,12 @@
 import { getDB } from './lib/index.mjs';
 import { DateTime } from "luxon";
 
-
-
-let now = Date.now();
 async function checker(){
+
+  let now = Date.now(); // timestamp
+  let dateNow = new Date(now);
+
+
   let db = await getDB();
   let minutesLater = function(now, minutes){
     return new Date(now).valueOf() + 60 *1000 * minutes;
@@ -15,22 +17,34 @@ async function checker(){
     }
   }).toArray().then(docs => {
     // console.log(docs)
-    console.log(new Date(docs[0].nextExecuteTime))
+    // console.log(new Date(docs[0].nextExecuteTime))
   });
 
   // https://moment.github.io/luxon/#/tour?id=your-first-datetime
-  const dt = DateTime.fromMillis(now);
+  // https://moment.github.io/luxon/api-docs/index.html#datetimeplus
+  // const dt = DateTime.fromMillis(now);
   // const minute = dt.minute;
-  let minute = 65;
-  let next10Minute = parseInt((minute + 10)/10) * 10;
-  let nextHour = 0;
-  if(next10Minute >= 60){
-    next10Minute = next10Minute % 60;
-    nextHour = 1
-  }else{
-    nextHour = 0;
-  }
-  console.log(minute, next10Minute, nextHour)
+  // let nowMinute = new Date(now).getMinutes();
+
+  let getNext10Minute = function(now, count=1){
+    let next10Minute = parseInt((new Date(now).getMinutes() + count*10)/10) * 10;
+    let nextHour = 0;
+    if(next10Minute >= 60){
+      next10Minute = next10Minute % 60;
+      nextHour = 1
+    }else{
+      nextHour = 0;
+    }
+    let next10MinuteTimestamp = new Date(now).setHours(
+        dateNow.getHours()+nextHour,
+        next10Minute,
+        0,
+        0
+      ); 
+    return [nextHour, next10Minute, next10MinuteTimestamp];
+  } 
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Date/setHours
+  console.log(new Date(getNext10Minute(now, 1)[2]), new Date(getNext10Minute(now, 2)[2]-1), dateNow)
 }
 
 
