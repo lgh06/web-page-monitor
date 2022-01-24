92376d94fea654f4171a762634a852ec412cb9c9
Daniel Liu <hnnk@qq.com>
2022-01-24 14:34:12 +0800

    comment

--- packages/helper/mongo/index.d.ts
+++ packages/helper/mongo/index.d.ts
@@ -5,6 +5,6 @@ export namespace mongo {
   function upsertDoc(db: Db, collectionName: string, doc: Document): Promise<ModifyResult<Document>>;
 }
 
-declare module '@webest/web-page-monitor-helper/mongo' {
-  function upsertDoc(db: Db, collectionName: string, doc: Document): Promise<ModifyResult<Document>>;
-}
+// declare module '@webest/web-page-monitor-helper/mongo' {
+//   function upsertDoc(db: Db, collectionName: string, doc: Document): Promise<ModifyResult<Document>>;
+// }
