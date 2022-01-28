48359fa42f384dc94a81d7e714bf0665203a03bc
Daniel Liu <hnnk@qq.com>
2022-01-28 18:08:58 +0800

    index

--- packages/worker-scheduler/taskHistoryChecker.mjs
+++ packages/worker-scheduler/taskHistoryChecker.mjs
@@ -23,10 +23,23 @@ async function taskHistoryChecker (db){
  */
 async function errorChecker (db){
   db = db || await getDB();
-  db.collection('taskHistory').find({
+  db.collection(collectionName).find({
     taskId: ObjectId('61f3a300410c05a636f632d9'),
-  }).sort({finishTime: -1}).limit(3).toArray().then(docs => {
+  }).sort({checked: 1, finishTime: -1}).limit(3).toArray().then(async docs => {
     console.log(docs)
+    if(docs && docs.length){
+      docs.forEach(async doc => {
+        let filter = {_id: doc._id};
+        if(!doc.checked){
+          // do something
+          await db.collection(collectionName).findOneAndUpdate(filter, {
+            $set: {
+              checked: 1,
+            }
+          })
+        }
+      })
+    }
   })
   // db.collection('taskHistory').watch().on('change', async function (change) {})
 }
