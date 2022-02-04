f019acffcc400e7503880b2294963cf41773ce72
Daniel Liu <hnnk@qq.com>
2022-02-04 14:51:18 +0800

    mongo docker

---
+++ packages/vendor-scripts-n-configs/docker_mongo/startMongo.bat
@@ -0,0 +1,3 @@
+docker volume create mongovolume  
+@rem https://docs.mongodb.com/v5.0/reference/program/mongod/#std-option-mongod.--bind_ip
+docker run -d --hostname my-host --name some-mongo -p 27017:27017 --log-driver local -v mongovolume:/data/db mongo:5-focal --bind_ip ::,0.0.0.0
