db8408fb93399a823e0748f81e7d7d99ffa11f2c
Daniel Liu <hnnk@qq.com>
2022-02-04 17:19:55 +0800

    add nextHost

--- packages/web/CONFIG.js
+++ packages/web/CONFIG.js
@@ -5,14 +5,15 @@ import { globalConfig } from "@webest/web-page-monitor-helper";
 let frontPort = String(process.env.NEXT_PUBLIC_DEV) === 'true' ? '3002' : '3001';
 console.log('frontPort',frontPort, process.env.NEXT_PUBLIC_DEV);
 let backPort = '3002';
+let host = globalConfig.nextHost ? globalConfig.nextHost : 'localhost';
 let CONFIG = {
   mongodbURI: '' || globalConfig.mongodbURI,
-  frontHost: `http://localhost:${frontPort}`,
-  backHost:`http://localhost:${backPort}`,
+  frontHost: `http://${host}:${frontPort}`,
+  backHost:`http://${host}:${backPort}`,
   giteeOauthClientId: '4606a3bd7aa3597e4de3e3c84687b8a2df672a6485cd112886cd1aee1cdc5d0e',
   giteeOauthClientSecret: '580f096526be60dcc2ef88bb933182918c2e2a6ab1f6880fbd54ac33db5a69a2',
-  giteeRedirectUri: `http://localhost:${backPort}/api/login?provider=gitee`,
-  socketio: `http://localhost:3003/`,
+  giteeRedirectUri: `http://${host}:${backPort}/api/login?provider=gitee`,
+  socketio: `http://${host}:3003/`,
 };
 
 export { CONFIG, CONFIG as default };
