d1ef623e50ce4b88ee6434583905ca85b3c33bc1
Daniel Liu <hnnk@qq.com>
2022-02-04 20:57:40 +0800

    comment

--- packages/vendor-scripts-n-configs/docker_keycloak/startKeycloak.bat
+++ packages/vendor-scripts-n-configs/docker_keycloak/startKeycloak.bat
@@ -1,3 +1,4 @@
 @rem local dev use
+@rem https://github.com/keycloak/keycloak-containers/blob/main/server/README.md
 docker volume create keycloakvolume
 docker run -e KEYCLOAK_USER=user -e KEYCLOAK_PASSWORD=passwd --log-driver local -v keycloakvolume:/opt/jboss -p 10691:8080 -d --restart=always jboss/keycloak
