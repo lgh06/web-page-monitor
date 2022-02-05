effbd76229212419832f17cf49b85e1f34b441c6
Daniel Liu <hnnk@qq.com>
2022-02-05 09:08:44 +0800

    add volume

--- packages/vendor-scripts-n-configs/docker_rabbitmq/startRabbitMQ.bat
+++ packages/vendor-scripts-n-configs/docker_rabbitmq/startRabbitMQ.bat
@@ -1,2 +1,3 @@
-docker volume create rabbitvolume  
-docker run -d --hostname my-host --name some-rabbit  -p 15672:15672 -p 5672:5672 -e RABBITMQ_DEFAULT_USER=guest -e RABBITMQ_DEFAULT_PASS=guest --log-driver local -v rabbitvolume:/var/lib/rabbitmq --restart=always ccr.ccs.tencentyun.com/lgh06/rabbitmq-delayed:0.0.1
+docker volume create rabbitvolumedata  
+docker volume create rabbitvolumeconfig  
+docker run -d --hostname my-host --name some-rabbit  -p 15672:15672 -p 5672:5672 -e RABBITMQ_DEFAULT_USER=guest -e RABBITMQ_DEFAULT_PASS=guest --log-driver local -v rabbitvolumedata:/var/lib/rabbitmq -v rabbitvolumeconfig:/etc/rabbitmq --restart=always ccr.ccs.tencentyun.com/lgh06/rabbitmq-delayed:0.0.2
