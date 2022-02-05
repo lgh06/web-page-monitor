cf5dc6d2d78d8a696f2614aca3fff4243f6f9b3b
Daniel Liu <hnnk@qq.com>
2022-02-05 16:01:42 +0800

    update shell

--- packages/vendor-scripts-n-configs/pptr/install_dep.sh
+++ packages/vendor-scripts-n-configs/pptr/install_dep.sh
@@ -1,1 +1,6 @@
+sudo add-apt-repository main
+sudo add-apt-repository universe
+sudo add-apt-repository restricted
+sudo add-apt-repository multiverse
+sudo apt-get update
 xargs -a dep.txt sudo apt-get install -y
