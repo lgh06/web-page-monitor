ec3622db82ba4ca6dbb06f8f3a470350c7771c2e
Daniel Liu <hnnk@qq.com>
2022-02-09 14:00:18 +0800

    regexp

--- packages/pptr/lib/eraser/weiboEraser.mjs
+++ packages/pptr/lib/eraser/weiboEraser.mjs
@@ -3,7 +3,7 @@
 
 async function weiboEraser({ result, taskDetail }) {
   if (taskDetail && taskDetail.pageURL && taskDetail.pageURL.includes('m.weibo.cn')) {
-    // erase 227（万）次播放
+    // erase 227万次播放
     result = result.replace(/\ \d+万?次播放\ /g, ' ');
     // console.log('step 1', result);
     // erase 转评赞
@@ -12,6 +12,13 @@ async function weiboEraser({ result, taskDetail }) {
     result = result.replace(/\ 昨天 \d{1,2}:\d{2}\ /g, ' ');
     // erase 1-15 05:19
     result = result.replace(/\ \d{1,2}-\d{1,2} \d{1,2}:\d{2} /g, ' ');
+    // erase 9小时前 3分钟前 刚刚 5:19
+    result = result.replace(/\ (\d{1,2}小时前|\d{1,2}分钟前|刚刚|\d{1,2}:\d{2})\ /g, ' ');
+    // erase 转 评 赞
+    result = result.replace(/\ (转发|\d+)\ (评论|\d+)\ (赞|\d+)\ /g, ' ');
+    // erase 2021-9-19
+    result = result.replace(/\ \d{4}-\d{1,2}-\d{1,2}\ /g, ' ');
+
   }
   return result
 }
