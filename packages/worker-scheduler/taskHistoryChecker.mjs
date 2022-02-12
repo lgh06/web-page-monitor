fef6971d1f2f1c0344db7233ad87c327ab003dd5
Daniel Liu <hnnk@qq.com>
2022-02-12 09:36:00 +0800

    nowCacheOnTask

--- packages/worker-scheduler/taskHistoryChecker.mjs
+++ packages/worker-scheduler/taskHistoryChecker.mjs
@@ -38,7 +38,7 @@ async function singleTaskHistoryChecker (taskDetail, db){
           // TODO send alert
           nowCacheOnTask = await diffNotifier(arr[index-1], doc, taskDetail, db);
           console.log('taskHistoryChecker cacheOnTask', nowCacheOnTask);
-          if(nowCacheOnTask && Object.keys(nowCacheOnTask).length){
+          if(nowCacheOnTask){
             await db.collection('task').updateOne({_id: new ObjectId(taskDetail._id)}, {
               $set: {
                 cache: nowCacheOnTask,
