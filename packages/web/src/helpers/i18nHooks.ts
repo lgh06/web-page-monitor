6c427606a833ff8fb5a33e631e8853aab257f741
Daniel Liu <hnnk@qq.com>
2022-02-12 12:17:07 +0800

    hooks

---
+++ packages/web/src/helpers/i18nHooks.ts
@@ -0,0 +1,11 @@
+import { useTranslation } from 'react-i18next';
+import { useRouter } from 'next/router';
+
+export function useI18n(){
+  const { t, i18n } = useTranslation();
+  const { locale, isReady } = useRouter();
+  i18n.changeLanguage(locale);
+  
+
+  return { t, i18n, locale, isReady }
+}
