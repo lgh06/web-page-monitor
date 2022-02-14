41f1e6b289441dc169393c93e542bfbf3f8d83c3
Daniel Liu <hnnk@qq.com>
2022-02-14 16:26:20 +0800

    market catch more routes

--- packages/web/src/pages/market/[[...slug]].tsx
+++ packages/web/src/pages/market/[[...slug]].tsx
@@ -3,9 +3,9 @@ import Head from 'next/head'
 // import Image from 'next/image'
 import styles from '../styles/modules/Home.module.scss'
 import Link from 'next/link'
-import { useI18n } from '../helpers'
+import { useI18n } from '../../helpers'
 import Cookies from 'js-cookie'
-import nextConfig from "../../next.config"
+import nextConfig from "../../../next.config"
 
 const Market: NextPage = () => {
   return (
