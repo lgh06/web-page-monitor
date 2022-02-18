a5b79aea7638908e7f575338a371aa26d9de7e05
Daniel Liu <hnnk@qq.com>
2022-02-18 16:46:35 +0800

    fix

--- packages/helper/jwt.mjs
+++ packages/helper/jwt.mjs
@@ -7,37 +7,21 @@ import fs from "fs"
 
 // https://nodejs.org/docs/latest-v16.x/api/crypto.html#static-method-keyobjectfromkey
 // https://github.com/panva/jose/blob/c185e24def279e921258ccbafaf65d4bb571d60d/docs/README.md
-// const { webcrypto, KeyObject } = await import('crypto');
-// const { subtle } = webcrypto;
-// const key = await subtle.generateKey({
-//   name: 'HMAC',
-//   hash: 'SHA-256',
-//   length: 256
-// }, true, ['sign', 'verify']);
 
-// const key = await subtle.generateKey({
-//   name: 'HMAC',
-//   hash: 'SHA-256',
-//   length: 256
-// }, true, ['sign', 'verify']);
-
-// const keyObject = KeyObject.from(key);
-// console.log(keyObject)
 
 // const { publicKey, privateKey } = await generateKeyPair('PS384')
-const publicKey = await importJWK(JSON.parse(fs.readFileSync('./pub', 'utf8')), 'PS384')
-const privateKey = await importJWK(JSON.parse(fs.readFileSync('./priv', 'utf8')), 'PS384')
-
-console.log(publicKey, privateKey)
+let { priv } = await import("./priv.mjs");
+let { pub } = await import("./pub.mjs");
+console.log(typeof pub)
+const publicKey = await importJWK(pub, 'PS384')
+const privateKey = await importJWK(priv, 'PS384')
 
 // const privateJwk = await exportJWK(privateKey)
 // const publicJwk = await exportJWK(publicKey)
 
-// fs.writeFileSync('./priv',JSON.stringify(privateJwk,null,2));
-// fs.writeFileSync('./pub',JSON.stringify(publicJwk, null, 2));
+// fs.writeFileSync('./priv.mjs',`export const priv =`+ JSON.stringify(privateJwk,null,2));
+// fs.writeFileSync('./pub.mjs',`export const pub =` +JSON.stringify(publicJwk, null, 2));
 // console.log(privateJwk, publicJwk)
-
-
 
 const jwt = await new SignJWT({ 'urn:example:claim': true })
   .setProtectedHeader({ alg: 'PS384' })
