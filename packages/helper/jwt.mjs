713e6dd6a69fbf165542a3c843fa5e107774a1ec
Daniel Liu <hnnk@qq.com>
2022-02-18 17:27:16 +0800

    jwt

--- packages/helper/jwt.mjs
+++ packages/helper/jwt.mjs
@@ -1,5 +1,5 @@
 import { SignJWT,
-  generateKeyPair, exportJWK,importJWK,
+  generateKeyPair, exportJWK,importJWK,exportPKCS8,
   decodeProtectedHeader,decodeJwt,
   jwtVerify,
 } from "jose";
@@ -8,20 +8,20 @@ import fs from "fs"
 // https://nodejs.org/docs/latest-v16.x/api/crypto.html#static-method-keyobjectfromkey
 // https://github.com/panva/jose/blob/c185e24def279e921258ccbafaf65d4bb571d60d/docs/README.md
 
-// below one line used only when generating keypair
-// const { publicKey, privateKey } = await generateKeyPair('PS384')
-
 // below 4 lines used when already have keypair
 let { priv } = await import("./priv.mjs");
 let { pub } = await import("./pub.mjs");
 const publicKey = await importJWK(pub, 'PS384')
 const privateKey = await importJWK(priv, 'PS384')
 
-// below 4 lines used only when generating keypair
+// below 7 lines used only when generating keypair
+// const { publicKey, privateKey } = await generateKeyPair('PS384')
 // const privateJwk = await exportJWK(privateKey)
 // const publicJwk = await exportJWK(publicKey)
+// const privatePKCS8 = await exportPKCS8(privateKey)
 // fs.writeFileSync('./priv.mjs',`export const priv =`+ JSON.stringify(privateJwk,null,2));
 // fs.writeFileSync('./pub.mjs',`export const pub =` +JSON.stringify(publicJwk, null, 2));
+// fs.writeFileSync('./privPKCS8.pk8', privatePKCS8);
 
 const jwt = await new SignJWT({ 'urn:example:claim': true })
   .setProtectedHeader({ alg: 'PS384' })
