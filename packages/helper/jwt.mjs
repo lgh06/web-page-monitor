16b88742a55b441d17bde262f351a86cac8b0526
Daniel Liu <hnnk@qq.com>
2022-02-18 16:49:58 +0800

    add comments

--- packages/helper/jwt.mjs
+++ packages/helper/jwt.mjs
@@ -8,20 +8,20 @@ import fs from "fs"
 // https://nodejs.org/docs/latest-v16.x/api/crypto.html#static-method-keyobjectfromkey
 // https://github.com/panva/jose/blob/c185e24def279e921258ccbafaf65d4bb571d60d/docs/README.md
 
-
+// below one line used only when generating keypair
 // const { publicKey, privateKey } = await generateKeyPair('PS384')
+
+// below 4 lines used when already have keypair
 let { priv } = await import("./priv.mjs");
 let { pub } = await import("./pub.mjs");
-console.log(typeof pub)
 const publicKey = await importJWK(pub, 'PS384')
 const privateKey = await importJWK(priv, 'PS384')
 
+// below 4 lines used only when generating keypair
 // const privateJwk = await exportJWK(privateKey)
 // const publicJwk = await exportJWK(publicKey)
-
 // fs.writeFileSync('./priv.mjs',`export const priv =`+ JSON.stringify(privateJwk,null,2));
 // fs.writeFileSync('./pub.mjs',`export const pub =` +JSON.stringify(publicJwk, null, 2));
-// console.log(privateJwk, publicJwk)
 
 const jwt = await new SignJWT({ 'urn:example:claim': true })
   .setProtectedHeader({ alg: 'PS384' })
