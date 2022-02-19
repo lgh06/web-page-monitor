89b546524657521f3cc4857436f7682fa8cd1ed8
Daniel Liu <hnnk@qq.com>
2022-02-19 10:40:05 +0800

    generate jwt keypair if not exist

--- packages/helper/helper.mjs
+++ packages/helper/helper.mjs
@@ -3,4 +3,6 @@ export * from "./cronTime.mjs";
 export * from "./sampleFunction.mjs";
 export * from "./globalConfig.mjs";
 export * from "./transRes.mjs";
+export * from "./pub.mjs";
+export * from "./priv.mjs";
 // export {mongo} from "./mongo.mjs";
--- packages/helper/jwt-standalone-gen-cert.mjs
+++ packages/helper/jwt-standalone-gen-cert.mjs
@@ -7,26 +7,33 @@ import { SignJWT,
   decodeProtectedHeader,decodeJwt,
   jwtVerify,
 } from "jose";
+import fs from "fs";
 // use jsrsasign only in web browser
 // import { KEYUTIL, jws } from "jsrsasign";
 
 // https://nodejs.org/docs/latest-v16.x/api/crypto.html#static-method-keyobjectfromkey
 // https://github.com/panva/jose/blob/c185e24def279e921258ccbafaf65d4bb571d60d/docs/README.md
 
-// below 4 lines used when already have keypair
-let { priv } = await import("./priv.mjs");
-let { pub } = await import("./pub.mjs");
-const publicKey = await importJWK(pub, 'PS384')
-const privateKey = await importJWK(priv, 'PS384')
 
-// below 7 lines used only when generating keypair
-// const { publicKey, privateKey } = await generateKeyPair('PS384')
-// const privateJwk = await exportJWK(privateKey)
-// const publicJwk = await exportJWK(publicKey)
-// const privatePKCS8 = await exportPKCS8(privateKey)
-// fs.writeFileSync('./priv.mjs',`export const priv =`+ JSON.stringify(privateJwk,null,2));
-// fs.writeFileSync('./pub.mjs',`export const pub =` +JSON.stringify(publicJwk, null, 2));
-// fs.writeFileSync('./privPKCS8.pk8', privatePKCS8);
+if(fs.existsSync('./priv.mjs') && fs.existsSync('./pub.mjs')){
+  // below 4 lines used when already have keypair
+  let { priv } = await import("./priv.mjs");
+  let { pub } = await import("./pub.mjs");
+  var publicKey = await importJWK(pub, 'PS384')
+  var privateKey = await importJWK(priv, 'PS384')
+}else{
+  // below 7 lines used only when generating keypair
+  const keyPair = await generateKeyPair('PS384')
+  var publicKey =  keyPair.publicKey;
+  var privateKey = keyPair.privateKey;
+  const privateJwk = await exportJWK(privateKey)
+  const publicJwk = await exportJWK(publicKey)
+  const privatePKCS8 = await exportPKCS8(privateKey)
+  fs.writeFileSync('./priv.mjs',`export const priv =`+ JSON.stringify(privateJwk,null,2));
+  fs.writeFileSync('./pub.mjs',`export const pub =` +JSON.stringify(publicJwk, null, 2));
+  fs.writeFileSync('./privPKCS8.pk8', privatePKCS8);
+}
+
 
 const jwt = await new SignJWT({ 'urn:example:claim': true })
   .setProtectedHeader({ alg: 'PS384' })
