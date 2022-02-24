f492269fc5678bba4d9978fc0c3c30b86bddc328
Daniel Liu <hnnk@qq.com>
2022-02-24 15:19:10 +0800

    apiPrefix use absolute path

--- packages/web/CONFIG.js
+++ packages/web/CONFIG.js
@@ -10,7 +10,7 @@ let CONFIG = {
   dbName: process.env.dbName || 'webmonitordb',
   mongodbURI: process.env.mongodbURI || "mongodb://localhost:27017/",
   apiHost: process.env.NEXT_PUBLIC_apiHost || ( host === 'localhost' ? `http://${host}:${backPort}` : `` ),
-  apiPrefix: process.env.NEXT_PUBLIC_apiPrefix || `/./api`,
+  apiPrefix: process.env.NEXT_PUBLIC_apiPrefix || `/api`,
   giteeOauthClientId: '4606a3bd7aa3597e4de3e3c84687b8a2df672a6485cd112886cd1aee1cdc5d0e',
   giteeOauthClientSecret: '580f096526be60dcc2ef88bb933182918c2e2a6ab1f6880fbd54ac33db5a69a2',
   socketio: `http://${host}:3003/`,
