a2936ff4879c17f46d6d23a68c3c056383c3eec2
Daniel Liu <hnnk@qq.com>
2022-02-25 10:53:05 +0800

    update

--- packages/vendor-scripts-n-configs/docker_caddy/restartCaddy.sh
+++ packages/vendor-scripts-n-configs/docker_caddy/restartCaddy.sh
@@ -1,2 +1,2 @@
 caddy_container_id=$(docker ps | grep caddy | awk '{print $1;}')
-docker exec -w /etc/caddy $caddy_container_id caddy restart
+docker exec -w /etc/caddy $caddy_container_id caddy stop && caddy start
