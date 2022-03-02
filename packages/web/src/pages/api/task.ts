b82f122778148ab5ed628d8616b3242bafdef5fd
Daniel Liu <hnnk@qq.com>
2022-03-02 22:40:22 +0800

    update

--- packages/web/src/pages/api/task.ts
+++ packages/web/src/pages/api/task.ts
@@ -90,7 +90,7 @@ async function _postHandler(
   let userTaskCount = await db.collection("task").countDocuments({
     userId,
   });
-  if(userTaskCount > 3){
+  if(userTaskCount >= 3){
     return res.status(400).json({ err: 'user task count is over 3' })
   }
   return mongo.upsertDoc(db, 'task', filter, newDoc, res, true);
