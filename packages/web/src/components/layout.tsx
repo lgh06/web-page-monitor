715f612661fb08d451836fa34831ef361e0921fe
Daniel Liu <hnnk@qq.com>
2022-03-20 17:31:19 +0800

    update baidu tongji

--- packages/web/src/components/layout.tsx
+++ packages/web/src/components/layout.tsx
@@ -115,8 +115,7 @@ export default function Layout({ children, statusCode }) {
         dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PTKMFSF');`,
         }}/>
-      <Script id="bd1"
-        strategy="afterInteractive"
+      <script id="bd1"
         dangerouslySetInnerHTML={{
           __html: `var _hmt = _hmt || [];
           (function() {
