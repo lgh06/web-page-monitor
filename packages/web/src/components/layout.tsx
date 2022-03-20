854e68c0fe402d652b8eccbb2fbcf8a1ca6a6f9e
Daniel Liu <hnnk@qq.com>
2022-03-20 17:05:51 +0800

    add google tag manager

--- packages/web/src/components/layout.tsx
+++ packages/web/src/components/layout.tsx
@@ -10,6 +10,7 @@ import { userInfoAtom } from '../atoms';
 import { useImmerAtom } from 'jotai/immer'
 import { useEffect, useState } from 'react'
 import { NavHeader } from "./navHeader";
+import Script from 'next/script'
 
 
 function Footer() {
@@ -109,9 +110,15 @@ export default function Layout({ children, statusCode }) {
         <link rel="icon" href="/favicon.ico" />
         <meta name="msvalidate.01" content="F18D53D95B52E2C08B800882D9946CA0" />
       </Head>
+      <Script id="gtag1"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PTKMFSF');`,
+        }}/>
       <NavHeader />
       {children}
       <Footer />
+      <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PTKMFSF"height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
     </>
   )
 }
