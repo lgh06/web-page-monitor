5078a6e1a88432c72e82bbdbb0c9e4ac29122ab8
Daniel Liu <hnnk@qq.com>
2022-03-22 19:04:41 +0800

    add tencent cloud error log

--- packages/web/src/components/layout.tsx
+++ packages/web/src/components/layout.tsx
@@ -121,6 +121,15 @@ export default function Layout({ children, statusCode }) {
         dangerouslySetInnerHTML={{
           __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PTKMFSF');`,
         }}/>
+      <Script defer id="tct1"
+        strategy="afterInteractive"
+        src="https://cdn-go.cn/aegis/aegis-sdk/latest/aegis.min.js"
+        />
+        <Script defer id="tct2"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(typeof window !=='undefined' && document.getElementById('tct1') ) ? document.getElementById('tct1').onload = () => {window.aegis = new Aegis({id: 'Ll387HjmVom9LP6Yv0',reportApiSpeed: true, reportAssetSpeed: true, spa: true})} : null`,
+        }}/>
       <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PTKMFSF" height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe></noscript>
     </>
   )
