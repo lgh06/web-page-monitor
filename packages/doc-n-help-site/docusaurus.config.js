e1f11f6d55ec951be0b58501b5e727304e267083
Daniel Liu <hnnk@qq.com>
2022-04-04 19:55:46 +0800

    doc sidebar config

--- packages/doc-n-help-site/docusaurus.config.js
+++ packages/doc-n-help-site/docusaurus.config.js
@@ -25,9 +25,9 @@ const config = {
         docs: {
           sidebarPath: require.resolve('./sidebars.js'),
           // Please change this to your repo.
-          // editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/lgh06/web-page-monitor/tree/main/packages/doc-n-help-site/',
           breadcrumbs: false,
-          // sidebarCollapsed: false,
+          sidebarCollapsed: false,
           routeBasePath:'/',
           showLastUpdateTime: true,
         },
