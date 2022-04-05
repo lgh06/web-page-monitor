6978cc0ad90daa0576a9677e0156cc9dd0305cab
Daniel Liu <hnnk@qq.com>
2022-04-05 20:22:36 +0800

    update script tag

---
+++ packages/doc-n-help-site/static/js/wpmt_doc_global_analyze.js
@@ -0,0 +1,18 @@
+(function () {
+
+  var bd = document.createElement('script'); bd.crossOrigin = 'anonymous'; bd.async = true;
+  bd.src = "https://hm.baidu.com/hm.js?155feb4af4ff86279f1fb8e01eebc1e6"; document.body.appendChild(bd);
+
+  (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-PTKMFSF');
+
+  (function () {
+    var tct = document.createElement('script'); tct.crossOrigin = 'anonymous';tct.async = true;
+    tct.onload = function () { window.aegis = new Aegis({ id: 'Ll387HjmVom9LP6Yv0', reportApiSpeed: true, reportAssetSpeed: true, spa: true, delay: 10000 }) }
+    tct.src = "https://cdn-go.cn/aegis/aegis-sdk/latest/aegis.min.js"; document.body.appendChild(tct);
+    (function (c, l, a, r, i, t, y) {
+      c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+      t = l.createElement(r); t.async = 1; t.src = "https://www.clarity.ms/tag/" + i + "?ref=bwt";
+      y = l.getElementsByTagName(r)[0]; y.parentNode.insertBefore(t, y);
+    })(window, document, "clarity", "script", "bahjgyjc7q");
+  })();
+})();
