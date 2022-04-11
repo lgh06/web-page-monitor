8cde0d2fb8bb578687dab00416bfff7fe85a91e1
Daniel Liu <hnnk@qq.com>
2022-04-11 11:16:03 +0800

    update a download

--- packages/web/src/helpers/frontHelpers.ts
+++ packages/web/src/helpers/frontHelpers.ts
@@ -101,7 +101,10 @@ export function arrayToCsv(array){
 
   // Create a link to download it
   var pom = document.createElement('a');
-  pom.href = url;
   pom.setAttribute('download', filename);
-  pom.click();
+  pom.href = url;
+  document.body.appendChild(pom)
+  pom.click()
+  URL.revokeObjectURL(pom.href)
+  document.body.removeChild(pom)
 }
